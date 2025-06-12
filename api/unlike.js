1bf74d98d618a0acaa309ef64d664823de3ad9cd
Muhammad Jamaluddin <116259085+jamaljml18@users.noreply.github.com>
2025-06-12 08:41:05 +0700

    Update: unlike.js

--- api/unlike.js
+++ api/unlike.js
@@ -16,7 +16,8 @@ export default async function handler(req, res) {
     return res.status(200).end();
   }
 
-  if (req.method !== "POST") return res.status(405).end();
+  if (req.method !== "DELETE") {
+  return res.status(405).json({ message: "Method not allowed" });
 
   const { id_user, nama_user, id_tempat, nama_tempat } = req.body;
 
